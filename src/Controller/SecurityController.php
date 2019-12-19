61f9143e748b4b6997a52a1bcafa0f42cfcbd831
tnrdll <tnrdll@gmail.com>
2019-12-19 11:32:45 +0300

    make:auth login - register ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,126 +2,35 @@
 
 namespace App\Controller;
 
-
-use App\Entity\PlacesToVisit;
-use App\Entity\User;
-use App\Form\UserType;
-use http\Env\Response;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
-/**
- * @Route("/security")
- */
 class SecurityController extends AbstractController
 {
-
-
-
     /**
-     * @Route("/list-users", name="list_users")
+     * @Route("/login", name="app_login")
      */
-    public function listUsers(){
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
-        $em = $this->getDoctrine()->getManager();
-
-        $users = $em->createQueryBuilder()
-            ->select('u')
-            ->from('App:User','u')
-            ->getQuery()
-            ->getArrayResult();
-
-        return new JsonResponse($users);
-
-    }
-
-
-    /**
-     * @Route("/login", name="login")
-     * @param AuthenticationUtils $authenticationUtils
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function login(AuthenticationUtils $authenticationUtils, Request $request){
-
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-
-        //her girişte currency euro olmalı. sistem euro üzerine kurulu
-        $session = $this->get('session');
-        $session->remove('currency');
-
-
-        return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error' => $error
-        ));
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-
 
     /**
-     * @Route("/register", name="register")
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/logout", name="app_logout")
      */
-    public function register(UserPasswordEncoderInterface $passwordEncoder, Request $request){
-
-        #Üye kaydı yapmıyoruz.
-
-
-        /*
-        $response = new \Symfony\Component\HttpFoundation\Response();
-        return new JsonResponse("kayit yapilamaz", $response::HTTP_FORBIDDEN);
-
-        */
-
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($password);
-            $user->setUsername($user->getEmail());
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            //return $this->redirectToRoute('login');
-
-            #automatic login after registration
-            $token = new UsernamePasswordToken(
-                $user,$user->getPlainPassword(), 'main', $user->getRoles()
-            );
-
-            $this->get('security.token_storage')->setToken($token);
-            $this->get('session')->set('_security_main',serialize($token));
-
-            // Fire the login event manually
-            $event = new InteractiveLoginEvent($request, $token);
-
-            $dispatcher = new EventDispatcher();
-            $dispatcher->dispatch('security.interactive_login',$event);
-
-
-            return $this->redirectToRoute('app_dashboard');
-
-        }
-
-        return $this->render('security/register.html.twig', array(
-            'form' => $form->createView()
-        ));
-
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
-
-
 }
